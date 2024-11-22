67bd1b40689e860c8d2f2872d6454ae4152f900f
Devanshu Gupta <devanshu9167@gmail.com>
2024-11-22 21:13:42 +0530

    #

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ windowlogo.addEventListener('click',()=>{
         windowbar.style.top="80px";
     }
 })
+let smallwindow=document.getElementById("smallwindow")
+let thispc=document.getElementById('thispc')
+thispc.addEventListener('dblclick',()=>{
+    smallwindow.style.display="block";
+})
+let close=document.getElementById('close')
+close.addEventListener('click',(e)=>{
+    smallwindow.style.display="none";
+})
+let max1=document.getElementById('max1')
+max1.addEventListener('click',()=>{
+   if( smallwindow.style.width=="100vw"){
+    smallwindow.style.width="40%"
+    smallwindow.style.height="60%"
+    smallwindow.style.top="100px"
+   }else{
+    smallwindow.style.width="100vw"
+    smallwindow.style.height="100vh"
+    smallwindow.style.top="0"
+   }
+})
+let spotify1=document.getElementById('spotify1')
+spotify1.addEventListener('dblclick',()=>{
+    smallwindow.style.display="block";
+})
